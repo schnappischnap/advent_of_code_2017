0cc9405668c47f75a065aed5d0ba0594483dfc1b
schnappischnap <benjaminianwatson@gmail.com>
2017-12-11 15:27:49 +0000

    Added day 10.

---
+++ day_11_hex_ed.py
@@ -0,0 +1,20 @@
+def calculate_steps(data):
+    blah = {"n": (0, 1),  "ne": (1, 0),  "se": (1, -1),
+            "s": (0, -1), "sw": (-1, 0), "nw": (-1, 1)}
+    x, y = 0, 0
+    max_distance = 0
+
+    for step in data.split(","):
+        x, y = x + blah[step][0], y + blah[step][1]
+        distance = (abs(x) + abs(y) + abs(-x-y)) // 2
+        max_distance = max(max_distance, distance)
+
+    return distance, max_distance
+
+
+if __name__ == '__main__':
+    with open("day_11_input.txt") as f:
+        inp = f.readlines()[0]
+        out = calculate_steps(inp)
+        print("Part 1 answer: " + str(out[0]))
+        print("Part 2 answer: " + str(out[1]))
